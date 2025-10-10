0b04999d562166855db5add9762a65bba4780d15
Lars Kroll <lkroll@Larss-MacBook-Pro.local>
2025-10-10 14:48:46 +0200

    add simple gameloop state tracking with debug menu

---
+++ gamejam/scripts/debug_menu.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+@onready var make_turn_btn: Button = $HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/MakeTurnBtn
+@onready var win_btn: Button = $HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/WinBtn
+@onready var start_btn: Button = $HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/StartBtn
+@onready var restart_btn: Button = $HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/RestartBtn
+
+func _ready() -> void:
+	start_btn.pressed.connect(func(): GameManager.start_game())
+	make_turn_btn.pressed.connect(func(): GameManager.drop_chip())
+	win_btn.pressed.connect(func(): GameManager.set_is_game_won_expression(true))
+	restart_btn.pressed.connect(func(): GameManager.restart_game())
---
+++ gamejam/scripts/game_manager.gd
@@ -0,0 +1,28 @@
+extends Node2D
+@onready var state_chart_debugger: MarginContainer = $StateChartDebugger
+@onready var state_chart: StateChart = $StateChart
+
+var currentPlayer = 1 # Player 1 and 2
+
+func _ready() -> void:
+	if not OS.is_debug_build():
+		state_chart_debugger.hide()
+
+func togglePlayer():
+	if currentPlayer == 1:
+		currentPlayer = 2
+	else:
+		currentPlayer = 1
+
+func start_game():
+	state_chart.send_event("start_game")
+
+func drop_chip():
+	state_chart.send_event("dropping_chip")
+
+func restart_game():
+	state_chart.set_expression_property("is_game_won", false)
+	state_chart.send_event("restart_game")
+
+func set_is_game_won_expression(is_game_won: bool):
+	state_chart.set_expression_property("is_game_won", is_game_won)
