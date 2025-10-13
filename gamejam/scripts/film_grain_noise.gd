97775e2b2bf83341fd2469cde082118c6ef891dd
Lars Kroll <lkroll@Larss-MacBook-Pro.local>
2025-10-13 10:23:36 +0200

    fix generating noise texture every frame

--- gamejam/scripts/film_grain_noise.gd
+++ gamejam/scripts/film_grain_noise.gd
@@ -2,5 +2,9 @@ extends TextureRect
 
 @onready var rand := RandomNumberGenerator.new()
 
-func _process(_delta):
-	texture.noise.seed = rand.randi()
+var time_passed := 0.0
+func _process(delta):
+	time_passed += delta
+	if time_passed > 0.2:
+		time_passed = 0
+		texture.noise.seed = rand.randi()
